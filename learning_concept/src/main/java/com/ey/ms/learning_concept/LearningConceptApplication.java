d07118cc0e824fe66ab14df7f99653e98f5a81ac
Saloni Jain <Saloni.Jain@in.ey.com>
2024-04-12 01:03:36 +0530

    Initial commit

---
+++ learning_concept/src/main/java/com/ey/ms/learning_concept/LearningConceptApplication.java
@@ -0,0 +1,13 @@
+package com.ey.ms.learning_concept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningConceptApplication.class, args);
+	}
+
+}
---
+++ learning_concept/src/test/java/com/ey/ms/learning_concept/LearningConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ey.ms.learning_concept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
